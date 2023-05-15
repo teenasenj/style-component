b3e0dfaab5be9ce61bda75f042b1e76a26d2782b
teenasenj <teenachatgpt@gmail.com>
2023-05-15 12:46:58 +0530

    styling

---
+++ projectstyle/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Button from './Button'
+  
+function App() {
+  return (
+    <div >
+      <h1>Have a good day</h1>
+      <Button> Click </Button>
+    </div>
+  );
+}
+  
+export default App;
---
+++ projectstyle/src/Button.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Button = styled.div`
+	width : 100px ;
+	color : white ;
+	background-color : purple;
+	margin-left:100%;
+	font-size:2rem;
+`
+
+
+export default Button;
